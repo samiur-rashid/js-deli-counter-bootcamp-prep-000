b183056009281ad56430befa9abe0a07d00c4af4
Samiur Rashid <samrashid@gmail.com>
2017-10-16 04:08:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var i = 0;
 function takeANumber(katzDeliLine, person) {
   katzDeliLine.push(person);
   i++;
-  return console.log("Welcome, " + person + ".  You are number " + i + " in line.")
+  console.log("Welcome, " + person + ".  You are number " + i + " in line.")
+  return katzDeliLine
 }
 
 function nowServing(katzDeliLine) {
